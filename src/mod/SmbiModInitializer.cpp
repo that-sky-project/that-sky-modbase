a5d7c62536c185485882e029a48d746d34eafc29
HTMonkeyG <3254339747@qq.com>
2026-04-26 18:42:44 +0800

    Thread pool implementation.

--- src/mod/SmbiModInitializer.cpp
+++ src/mod/SmbiModInitializer.cpp
@@ -1,5 +1,92 @@
+#include <thread>
+#include <vector>
+#include <algorithm>
 #include "ModInternal.hpp"
 #include "mod/SmbiModInitializer.hpp"
+
+#undef max
+
+class SmbiModInitThreadPool: public SmbiMutexObject {
+  using PCInit = const SmbiModInitializer *;
+  using TaskPayload = std::vector<PCInit>;
+  using Task = std::pair<std::thread, TaskPayload>;
+
+public:
+  SmbiModInitThreadPool() = default;
+
+  ~SmbiModInitThreadPool() { Terminate(); }
+
+  void Initialize() {
+    m_index = 0;
+    u32 concurrency = std::thread::hardware_concurrency();
+    m_concurrency = std::max(2u, concurrency) - 1;
+    m_tasks.resize(m_concurrency);
+    m_running = false;
+  }
+
+  void Terminate() {
+    std::vector<std::thread> threads;
+    {
+      auto lock = LockRW();
+      for (auto &task: m_tasks) {
+        if (task.first.joinable())
+          threads.push_back(std::move(task.first));
+      }
+      m_tasks.clear();
+    }
+    for (auto &t: threads) {
+      if (t.joinable())
+        t.join();
+    }
+
+    m_running = false;
+  }
+
+  void Submit(
+    const SmbiModInitializer *task
+  ) {
+    auto lock = LockRW();
+
+    if (m_running)
+      return;
+
+    m_tasks[m_index].second.push_back(task);
+    m_index++;
+    m_index %= m_concurrency;
+  }
+
+  void Start() {
+    auto lock = LockRW();
+
+    if (m_running)
+      return;
+
+    m_running = true;
+
+    for (auto &task: m_tasks) {
+      task.first = std::thread(m_ThreadFunc, &task.second);
+    }
+  }
+
+  void StartSync() {
+    Start();
+    Terminate();
+  }
+
+private:
+  static void m_ThreadFunc(
+    const TaskPayload *funcs
+  ) {
+    for (auto &fn: *funcs) {
+      (*fn)(hModuleDll);
+    }
+  }
+
+  u32 m_concurrency;
+  u32 m_index;
+  std::vector<Task> m_tasks;
+  bool m_running;
+};
 
 HTStatus SmbiModInitializer::SetupAll(
   HMODULE hModuleDll
